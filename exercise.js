c79b87383ffe45de6e23faf671bee27e9156e755
dat-iro <138441382+dat-iro@users.noreply.github.com>
2024-01-10 01:19:39 +0100

    solution exercise 1

---
+++ exercise.js
@@ -0,0 +1,11 @@
+function sum(number1, number2) {
+  return number1 + number2
+}
+
+const sum2 = (number1, number2) => {
+  return number1 + number2
+}
+
+console.log("Prima operazione: ", sum(5,6));
+
+console.log("Seconda operazione: ", sum(5,6));
