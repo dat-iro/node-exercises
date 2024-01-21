36f05255fdd97a1a11f182ab4c6614074b44ed3d
dat-iro <138441382+dat-iro@users.noreply.github.com>
2024-01-21 18:23:09 +0100

    solution for exercise 8

--- NodejsProject/index.js
+++ NodejsProject/index.js
@@ -1,20 +1,12 @@
-import figlet from "figlet";
+import fs from "fs";
 
-figlet.text(
-  "Hello World!",
-  {
-    font: "Crazy",
-    horizontalLayout: "default",
-    verticalLayout: "default",
-    width: 100,
-    whitespaceBreak: true,
-  },
-  function (err, data) {
-    if (err) {
-      console.log("Something went wrong...");
-      console.dir(err);
-      return;
-    }
-    console.log(data);
+let data = "This is a test";
+
+fs.writeFile("test.txt", data, (err) => {
+  if (err) console.log(err);
+  else {
+    console.log("File written successfully\n");
+    console.log("The written has the following contents:");
+    console.log(fs.readFileSync("test.txt", "utf8"));
   }
-);
+});
